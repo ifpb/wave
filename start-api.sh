bc001b8c4245ac904fb43622884505fe5d30dd0d
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2023-03-17 17:23:03 -0300

    adjusting start-api.sh and .gitignoring

--- start-api.sh
+++ start-api.sh
@@ -7,27 +7,25 @@ dpkg --list | grep "venv" > /dev/null
 if [ $? -ne 0 ]; then
    echo -e "❌  package python3-venv is not installed!\n"
    exit 1
-else
+fi
 
-   if [ -d venv ]; then	
-      
-      echo -e "🐍  Activating Python virtual environment... "
-      source venv/bin/activate
-      pip freeze | grep -E "flask-restx" > /dev/null
-      if [ $? -ne 0 ]; then
-         echo "📦  Installing API dependencies... "
-         pip3 install flask flask-restx dotenv > /dev/null
-      fi
-   else
-      echo "🕒 Craeting Python virtual environment... "
-      python3 -m venv venv
-      echo -e "🐍  Activating Python virtual environment... "
-      source venv/bin/activate
-      
+if [ -d venv ]; then	
+   
+   echo -e "🐍  Activating Python virtual environment... "
+   source venv/bin/activate
+   pip3 freeze | grep -E "flask-restx" > /dev/null
+   if [ ! $? -ne 0 ]; then
       echo "📦  Installing API dependencies... "
-      pip3 install flask flask-restx  dotenv > /dev/null 
+      pip3 install flask flask-restx python-dotenv requests > /dev/null
    fi
-
+else
+   echo "🕒 Craeting Python virtual environment... "
+   python3 -m venv venv
+   echo -e "🐍  Activating Python virtual environment... "
+   source venv/bin/activate
+   
+   echo "📦  Installing API dependencies... "
+   pip3 install flask flask-restx python-dotenv requests > /dev/null 
 fi
 
 echo -e "🔛  Start API in port 8181\n"
